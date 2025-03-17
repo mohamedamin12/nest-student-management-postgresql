879dbc86f0f0292e9074755975888a24b4d4247f
mohamedamin12 <aboamin909080@gmail.com>
2025-03-17 04:43:06 +0200

    feat: integrate @nestjs/jwt for authentication and configure global validation pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,37 @@
-import { Module } from '@nestjs/common';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { StudentModule } from "./student/student.module";
+import { CourseModule } from "./course/course.module";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { AuthModule } from "./auth/auth.module";
+import { PassportModule } from "@nestjs/passport";
+import { JwtModule } from "@nestjs/jwt";
+import { JwtStrategy } from "./auth/jwt.strategy";
 import { UserModule } from './user/user.module';
-import { StudentModule } from './student/student.module';
-import { CourseModule } from './course/course.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, StudentModule, CourseModule, AuthModule],
-
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: "postgres",
+        port: configService.get<number>("PG_PORT"),
+        username: configService.get<string>("PG_USERNAME"),
+        password: configService.get<string>("PG_PASSWORD"),
+        database: configService.get<string>("PG_DATABASE"),
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+    CourseModule,
+    StudentModule,
+    AuthModule,
+    PassportModule,
+    JwtModule.register({ secret: "secrete", signOptions: { expiresIn: "1h" } }),
+    UserModule,
+  ],
+  providers:[JwtStrategy]
 })
 export class AppModule {}
